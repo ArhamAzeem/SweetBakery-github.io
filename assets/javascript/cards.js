a26689ef561da9421a3a1f52a69728d747c0a8dc
ArhamAzeem <arhamazeem318@gmail.com>
2024-07-01 23:43:11 +0500

    Upload

---
+++ assets/javascript/cards.js
@@ -0,0 +1,31 @@
+var cards=[
+    {"card_id":"1","title":"Nimcos Mix Nimco","price":"0.7$","des":"A unique fusion of dry noodles,pulses and nuts.","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Mix-Nimco.jpg"},
+    {"card_id":"2","title":"Nimcos Daal Moong","price":"0.5$","des":"Crunchy & savory fried Moong pulse with a feel of lightness.","pic":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXbwMNya515AiM0PWdeunJ0spu6YQATHMpxw&usqp=CAU"},
+    {"card_id":"3","title":"Nimcos Daal Chana","price":"1.3$","des":"Every piece is generously infused with a lively mix of spices and Chana pulse.","pic":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDUf-mL5uwsocM3nyXL54Uj2645RASlcMawA&usqp=CAU"},
+    {"card_id":"4","title":"Nimcos Daal Mooth","price":"1.2$","des":"Tantalizing blend of Masoor pulse and dry noodles.","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"}
+  
+];
+
+var cards1=[
+    {"title":"Brown Bread","price":"1.2","des":"Our Brown Bread is made with top-quality ingredients to ensure a wholesome and nutritious bread with a rich and hearty flavor. Medium, 17 Pcs","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Mix-Nimco.jpg"},
+    {"title":"Multigrain Bread","price":"1$","des":"Experience the goodness of our Multigrain Bread, made with high-quality grains and premium ingredients for a nutritious and flavorful choice. Medium, 17 Pcs","pic":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXbwMNya515AiM0PWdeunJ0spu6YQATHMpxw&usqp=CAU"},
+    {"title":"Sandwich Bread Plain","price":"0.8$","des":"Our Plain Sandwich Bread is a classic choice, made with high-quality ingredients for a versatile and soft option, perfect for sandwiches and toasts. Large, 24 Pcs","pic":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDUf-mL5uwsocM3nyXL54Uj2645RASlcMawA&usqp=CAU"},
+    {"title":"Pitta Bread","price":"0.7$","des":"(pkt) Savor the soft and chewy texture of our Pitta Bread, made with top-quality ingredients and conveniently packed in a packet for an authentic taste.","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"},
+    {"title":"Pitta Brown Bread","price":"2$","des":"(Pkt) Experience the goodness of our Brown Pitta Bread, made with high-quality ingredients and conveniently packed in a packet for an authentic and wholesome option.","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"},
+    {"title":"Garlic Bread","price":"1.3$","des":"Our Garlic Bread is a savory delight, made with high-quality ingredients and a delicious garlic spread for a flavorful experience.","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"}
+  
+];
+
+var cards2=[
+    {"title":"","price":"$","des":"","pic":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXbwMNya515AiM0PWdeunJ0spu6YQATHMpxw&usqp=CAU"},
+    {"title":"","price":"$","des":"","pic":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXbwMNya515AiM0PWdeunJ0spu6YQATHMpxw&usqp=CAU"},
+    {"title":"","price":"$","des":"","pic":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDUf-mL5uwsocM3nyXL54Uj2645RASlcMawA&usqp=CAU"},
+    {"title":"","price":"$","des":"","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"},
+    {"title":"","price":"$","des":"","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"},
+    {"title":"","price":"$","des":"","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"},
+    {"title":"","price":"$","des":"","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"},
+    {"title":"","price":"$","des":"","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"},
+    {"title":"","price":"$","des":"","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"},
+    {"title":"","price":"$","des":"","pic":"https://cliftonnimco.com/wp-content/uploads/2018/04/Dal-Moth.jpg"}
+  
+];
